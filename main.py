b108a90e5c7ad4a0129cabbd50179736442f5b50
David <masemoyv1@gmail.com>
2024-10-27 13:08:27 -0600

    API con método inicial

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify, request
+import mysql.connector as cnx
+
+app = Flask(__name__)
+
+config = {
+    "host": "localhost",
+    "port": 3306,
+    "user": "david",
+    "password": "1234",
+    "database": "api_matchpet"
+}
+cnxx = cnx.connect(**config)
+
+@app.route("/")
+def index():
+    return "ola sin h"
+
+@app.route("/user/<idUser>")
+def get_user(idUser):
+    
+    cur = cnxx.cursor()
+    cur.execute("SELECT * FROM user WHERE idUser = %s", (idUser,))
+    user = cur.fetchone()
+    cur.close()
+      
+    if user:
+        columns = [
+            "idUser", "email", "pass", "name", "lastName1", "lastName2", 
+            "profileImage", "userAge", "state", "city", "socialNetwork", 
+            "description", "created_at", "updated_at", "deleted_at"
+        ]
+        user_data = dict(zip(columns, user))
+        return jsonify(user_data)
+    return jsonify({"error": "User not found"}), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
